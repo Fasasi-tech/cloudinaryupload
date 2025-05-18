6a6e557257084fc49e7f5757b76e46cb6c61a10a
Ridwan <74691968+Fasasi-tech@users.noreply.github.com>
2025-05-18 22:21:51 +0100

    added limit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require("express");
 const app = express()
 const multer = require('multer');
 const dotenv = require('dotenv');
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 dotenv.config();
 //const storage = multer.memoryStorage();
 //const upload = multer({ storage: storage });
